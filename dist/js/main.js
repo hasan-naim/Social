0a5b2808c0794f90f53386b4a2c40bd3cbe783d5
hasanNaim6246 <hasannaim6246@gmail.com>
2021-09-11 20:15:45 +0600

    added go to github page of this project

--- dist/js/main.js
+++ dist/js/main.js
@@ -5,3 +5,24 @@ const warningDiv = document.querySelector(".warning-div")
 closeBtn.addEventListener("click", () => {
     warningDiv.classList.add("display-hide");
 });
+
+
+// code for go to github to this project
+
+
+const goToGithubSection = document.querySelector(".go_to_github");
+const goToGithubBtn = document.querySelector(".go_to_github_btn");
+const githubExitBtn = document.querySelector(".exitBtn");
+
+githubExitBtn.addEventListener("click", exitGithubSection)
+
+
+function exitGithubSection() {
+	goToGithubSection.classList.add("display_hide");
+
+	setTimeout(() => {
+		goToGithubBtn.classList.remove("opacity-0")
+		goToGithubBtn.classList.add("opacity-1")
+	}, 5000)
+
+}
